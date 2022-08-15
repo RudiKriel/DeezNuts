031b54a52abfc9bba99e6e75457fb65f29691276
rudi <rudi@comprsa.com>
2022-08-15 16:47:29 +0200

    End of Section 3

--- DeezNuts/ClientApp/src/app/app.component.ts
+++ DeezNuts/ClientApp/src/app/app.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = 'DeezNuts';
+  users: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: response => this.users = response,
+      error: error => console.log(error)
+    });
+  }
 }
